80b20ccf7014966fe9840de8b5df18995a2744de
SynchronClover <fernanmc@rose-hulman.edu>
2020-08-09 23:06:31 -0400

    Starting on Project

--- Project-Wrath_of_the_Polish_Hammer/Polish_Shoot_Game.py
+++ Project-Wrath_of_the_Polish_Hammer/Polish_Shoot_Game.py
@@ -1,17 +1,22 @@
 import pygame
 import sys
 
-pygame.init()
-pygame.display.set_caption("The Polish Hammer")
-screen = pygame.display.set_mode((1000, 700))
 
-while True:
-    for event in pygame.event.get():
-        print(event)  # Used for an example here
-        if event.type == pygame.QUIT:
-            sys.exit()
-        # Additional interactions
+def Main():
+    pygame.init()
+    pygame.display.set_caption("The Polish Hammer")
+    screen = pygame.display.set_mode((1000, 700))
 
-    # Draw things on the screen
+    while True:
+        for event in pygame.event.get():
+            print(event)  # Used for an example here
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # Additional interactions
 
-    pygame.display.update()
+        # Draw things on the screen
+
+        pygame.display.update()
+
+
+Main()
